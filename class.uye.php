2246c222ca81f099cd1dad0cc291e41a04bffa33
batuhansaygili <batuhansaygili94@gmail.com>
2016-05-28 01:52:10 +0300

    Listeleme Aktif

--- class.uye.php
+++ class.uye.php
@@ -9,25 +9,31 @@
 class uye {
 
     public $veriler;
-    public $sorgu;
-    function kayit($query,$kullanici_adi,$sifre,$email,$ad,$soyad){
+    public $dongu;
+    function kayit($db,$query,$kullanici_adi,$sifre,$email,$ad,$soyad){
 
         //$this->kullanici_adi = $kullanici_adi;
         //$this->sifre = $sifre;
         //$this->email = $email;
         //$this->ad = $ad;
         //$this->soyad = $soyad;
-        $this->sorgu->query;
+
 
         $this ->veriler[] = array('kullanici_adi' => $kullanici_adi,'sifre' => $sifre,'email' => $email , 'ad' => $ad , 'soyad' => $soyad );
 
 
     }
 
-    function listele(){
+    function listele($db,$query){
         //print_r($this->veriler);
-        foreach($this->veriler as $no => $uye) {
-            echo $uye['kullanici_adi'].'<br>';
+        //foreach($this->veriler as $no => $uye) {
+        ////    echo $uye['kullanici_adi'].'<br>';
+       // }
+        $dongu = $db->query($query);
+        if ( $dongu->rowCount() ){
+            while( $data = $dongu->fetch( PDO::FETCH_ASSOC ) ){
+                echo $data['kadi']."<br />";
+            }
         }
     }
 
--- index.php
+++ index.php
@@ -5,8 +5,9 @@
  * Date: 27.05.2016
  * Time: 21:28
  */
+include 'connect.php';
 require ('class.uye.php');
 $uyelik = new uye();
-$uyelik->kayit('batukan','123456','batuhansaygili@hotmail.com.tr','batuhan','saygılı');
-$uyelik->kayit('batukan1','1234561','batuhansaygili@hotmail.com.tr','batuhan2','saygılı2');
-$uyelik->listele();
+$uyelik->kayit($db,'SELECT * FROM uye','batukan','123456','batuhansaygili@hotmail.com.tr','batuhan','saygılı');
+//$uyelik->kayit('batukan1','1234561','batuhansaygili@hotmail.com.tr','batuhan2','saygılı2');
+$uyelik->listele($db,'SELECT * FROM uye');
