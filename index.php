8250fdc125c4fa1b868df6dd3053d4413c308283
batuhansaygili <batuhansaygili94@gmail.com>
2016-05-28 01:58:05 +0300

    Açıklama satırları eklendi.

--- index.php
+++ index.php
@@ -5,9 +5,15 @@
  * Date: 27.05.2016
  * Time: 21:28
  */
+// SQL Bağlantımızı Kuruyoruz
 include 'connect.php';
+// Sınıfımızı Dahil Ediyoruz
 require ('class.uye.php');
+// Yeni Sınıf Oluşturuyoruz
 $uyelik = new uye();
+
 $uyelik->kayit($db,'SELECT * FROM uye','batukan','123456','batuhansaygili@hotmail.com.tr','batuhan','saygılı');
 //$uyelik->kayit('batukan1','1234561','batuhansaygili@hotmail.com.tr','batuhan2','saygılı2');
+//$db --> SQL bağlantımızı sınıfa yolluyoruz ilk parametrede
+//İkinci parametrede sql select komutumuzu yazıp üyeleri listeliyoruz
 $uyelik->listele($db,'SELECT * FROM uye');
