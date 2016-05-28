4ffecf45c209942633e85883d3b94a78f6a1cda7
batuhansaygili <batuhansaygili94@gmail.com>
2016-05-28 04:16:42 +0300

    Açıklama satırları eklendi.

--- class.uye.php
+++ class.uye.php
@@ -44,7 +44,10 @@ class uye {
                 return 'Bu isim ile kayıtlı kullanıcı bullunmaktadır. <br>';
             }else if($dongu2->rowCount() >0){
                 return 'Bu email ile kayıtlı kullanıcı bullunmaktadır. <br>';
-            }else{
+            }else if(!valid_email($email)){
+                return 'Geçerli bir eposta giriniz';
+            }
+            else{
                 if(strlen($sifre) >= 6){
                     return 'Şifreniz 6 ve 6 karakterden büyük olmak zorundadır ';
                 }else if($sifre == '123456' or $sifre == 'qwert' or $sifre == '111111' or $sifre == '123456789' or $sifre == '12345678'){
@@ -70,4 +73,8 @@ class uye {
         }
     }
 
+    protected  function valid_email( $str )
+    {
+        return ( ! preg_match ( "/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix", $str ) ) ? FALSE : TRUE;
+    }
 }
